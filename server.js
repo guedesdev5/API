a2ee2ff6eea999456998ad26351c6a5beb843669
guedes5 <guedesdev5@gmail.com>
2024-07-11 04:27:24 -0300

    #Dev3 Atualizando o banco e criando rotas de vendedores

--- server.js
+++ server.js
@@ -7,26 +7,12 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-app.get('/login/:username/:senha', async (req, res) => {
-    try {
-        const todosVendedores = await prisma.vendedores.findMany();
-
-       
-            res.status(200).json({
-                message: 'Login encontrado com sucesso!',
-                status: 0
-            });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            error: 'Erro ao procurar login.',
-            status: 1
-        });
-    }
-});
+app.get('/apiGerenciamento', (req, res) =>{
+    res.send('Inicializado com sucesso...')
+})
 
 //post categorias
-app.post('/categorias', async (req, res) => {
+app.post('/apiGerenciamento/categorias', async (req, res) => {
     try {
         await prisma.Categorias.create({
             data: {
@@ -50,7 +36,7 @@ app.post('/categorias', async (req, res) => {
 });
 
 //put categorias
-app.put('/categorias/:id', async (req, res) => { 
+app.put('/apiGerenciamento/categorias/:id', async (req, res) => { 
     try {
         await prisma.Categorias.update({
             where: {
@@ -73,7 +59,7 @@ app.put('/categorias/:id', async (req, res) => {
 })
 
 //delete categorias
-app.delete('/categorias/:id', async (req, res) => {
+app.delete('/apiGerenciamento/categorias/:id', async (req, res) => {
     
     try{
         await prisma.Categorias.delete({
@@ -92,7 +78,8 @@ app.delete('/categorias/:id', async (req, res) => {
     }
 })
 
-app.get('/categorias', async (req, res) => {
+//get categorias
+app.get('/apiGerenciamento/categorias', async (req, res) => {
     try{
         const categorias = await prisma.Categorias.findMany()
         res.status(200).json({message: 'Sucesso ao acessar dados',
@@ -109,4 +96,87 @@ app.get('/categorias', async (req, res) => {
     
 })
 
-app.listen(8500)
+app.post('/apiGerenciamento/vendedores', async (req, res) => {
+    try {
+        await prisma.vendedores.create({
+            data: {
+                nome: req.body.nome,
+                username: req.body.username,
+                email: req.body.email,
+                senha: req.body.senha,
+                permissao: req.body.permissao
+            },
+        });
+        res.status(201).json({message: 'Dados inseridos corretamente',
+            status: 0
+        });
+    } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erro ao inserir os dados.',
+                status: 1
+             });
+    }
+});
+
+app.put('/apiGerenciamento/vendedores/:id', async (req, res) => { 
+    try {
+        await prisma.vendedores.update({
+            where: {
+                id: req.params.id
+            },
+            data: {
+                nome: req.body.nome,
+                username: req.body.username,
+                email: req.body.email,
+                senha: req.body.senha,
+                permissao: req.body.permissao
+            }
+        })
+        res.status(201).json({message: 'Dados atualizados!',
+            status: 0
+        })
+    }catch (error){
+        console.error(error);
+            res.status(500).json({ error: 'Erro ao atualizar os dados.',
+                status: 1
+             });
+    }   
+})
+
+app.delete('/apiGerenciamento/vendedores/:id', async (req, res) => {
+    
+    try{
+        await prisma.vendedores.delete({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.status(201).json({ message: "Usuário deletado com sucesso!",
+            status: 0
+        })
+    } catch (error){
+        console.error(error);
+            res.status(500).json({ error: 'Erro ao deletar categoria.',
+                status: 1
+             });
+    }
+})
+
+app.get('/apiGerenciamento/vendedores', async (req, res) => {
+    try{
+        const vendedores = await prisma.vendedores.findMany()
+        res.status(200).json({message: 'Sucesso ao acessar dados',
+            status: 0,
+            data:vendedores
+        })
+    } catch (error){
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao pegar dados das categoria.',
+            status: 1
+         });
+    }
+    
+    
+})
+
+app.listen(8500, () => console.log("API no ar........"))
